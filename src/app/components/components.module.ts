d9c574282d86e7ae851760c221ccf99b20577cec
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-05-04 18:20:05 -0300

    style: add prettier style

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,15 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserDetailsComponent } from './components/user-details/user-details.component';
 
-
-
 @NgModule({
-  declarations: [
-    UserDetailsComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: []
+  declarations: [UserDetailsComponent],
+  imports: [CommonModule],
+  exports: [UserDetailsComponent],
 })
-export class ComponentsModule { }
+export class ComponentsModule {}
