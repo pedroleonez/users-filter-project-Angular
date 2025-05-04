f6aee9f6979fc70f97e478c61dd0b3b9c769a182
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-05-04 18:18:58 -0300

    refactor: remove styleUrl

--- src/app/components/components/user-details/user-details.component.ts
+++ src/app/components/components/user-details/user-details.component.ts
@@ -3,8 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-user-details',
   standalone: false,
-  templateUrl: './user-details.component.html',
-  styleUrl: './user-details.component.css'
+  templateUrl: './user-details.component.html'
 })
 export class UserDetailsComponent {
 
